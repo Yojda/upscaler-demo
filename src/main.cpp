f0d630d971fc37d5ff0bcd7cb2833b0be6d451ce
Yojda <yanthekill@gmail.com>
2025-10-03 11:56:20 -0400

    [fix] texture on cube

--- src/main.cpp
+++ src/main.cpp
@@ -57,20 +57,57 @@ int main() {
 
     // Cube setup
     float cubeVertices[] = {
-        // positions
-        -0.5f,-0.5f,-0.5f,  0.5f,-0.5f,-0.5f,  0.5f, 0.5f,-0.5f,
-         0.5f, 0.5f,-0.5f, -0.5f, 0.5f,-0.5f, -0.5f,-0.5f,-0.5f,
-        -0.5f,-0.5f, 0.5f,   0.5f,-0.5f, 0.5f,  0.5f, 0.5f, 0.5f,
-         0.5f, 0.5f, 0.5f,  -0.5f, 0.5f, 0.5f, -0.5f,-0.5f, 0.5f,
-        -0.5f, 0.5f, 0.5f,  -0.5f, 0.5f,-0.5f, -0.5f,-0.5f,-0.5f,
-        -0.5f,-0.5f,-0.5f,  -0.5f,-0.5f, 0.5f,  -0.5f, 0.5f, 0.5f,
-         0.5f, 0.5f, 0.5f,   0.5f, 0.5f,-0.5f,  0.5f,-0.5f,-0.5f,
-         0.5f,-0.5f,-0.5f,   0.5f,-0.5f, 0.5f,  0.5f, 0.5f, 0.5f,
-        -0.5f,-0.5f,-0.5f,   0.5f,-0.5f,-0.5f,  0.5f,-0.5f, 0.5f,
-         0.5f,-0.5f, 0.5f,  -0.5f,-0.5f, 0.5f, -0.5f,-0.5f,-0.5f,
-        -0.5f, 0.5f,-0.5f,   0.5f, 0.5f,-0.5f,  0.5f, 0.5f, 0.5f,
-         0.5f, 0.5f, 0.5f,  -0.5f, 0.5f, 0.5f, -0.5f, 0.5f,-0.5f
+        // positions          // texcoords
+        // Front face
+        -0.5f,-0.5f, 0.5f,   0.0f, 0.0f,
+         0.5f,-0.5f, 0.5f,   1.0f, 0.0f,
+         0.5f, 0.5f, 0.5f,   1.0f, 1.0f,
+         0.5f, 0.5f, 0.5f,   1.0f, 1.0f,
+        -0.5f, 0.5f, 0.5f,   0.0f, 1.0f,
+        -0.5f,-0.5f, 0.5f,   0.0f, 0.0f,
+
+        // Back face
+        -0.5f,-0.5f,-0.5f,   1.0f, 0.0f,
+         0.5f,-0.5f,-0.5f,   0.0f, 0.0f,
+         0.5f, 0.5f,-0.5f,   0.0f, 1.0f,
+         0.5f, 0.5f,-0.5f,   0.0f, 1.0f,
+        -0.5f, 0.5f,-0.5f,   1.0f, 1.0f,
+        -0.5f,-0.5f,-0.5f,   1.0f, 0.0f,
+
+        // Left face
+        -0.5f,-0.5f,-0.5f,   0.0f, 0.0f,
+        -0.5f,-0.5f, 0.5f,   1.0f, 0.0f,
+        -0.5f, 0.5f, 0.5f,   1.0f, 1.0f,
+        -0.5f, 0.5f, 0.5f,   1.0f, 1.0f,
+        -0.5f, 0.5f,-0.5f,   0.0f, 1.0f,
+        -0.5f,-0.5f,-0.5f,   0.0f, 0.0f,
+
+        // Right face
+         0.5f,-0.5f,-0.5f,   1.0f, 0.0f,
+         0.5f,-0.5f, 0.5f,   0.0f, 0.0f,
+         0.5f, 0.5f, 0.5f,   0.0f, 1.0f,
+         0.5f, 0.5f, 0.5f,   0.0f, 1.0f,
+         0.5f, 0.5f,-0.5f,   1.0f, 1.0f,
+         0.5f,-0.5f,-0.5f,   1.0f, 0.0f,
+
+        // Top face
+        -0.5f, 0.5f,-0.5f,   0.0f, 1.0f,
+         0.5f, 0.5f,-0.5f,   1.0f, 1.0f,
+         0.5f, 0.5f, 0.5f,   1.0f, 0.0f,
+         0.5f, 0.5f, 0.5f,   1.0f, 0.0f,
+        -0.5f, 0.5f, 0.5f,   0.0f, 0.0f,
+        -0.5f, 0.5f,-0.5f,   0.0f, 1.0f,
+
+        // Bottom face
+        -0.5f,-0.5f,-0.5f,   0.0f, 0.0f,
+         0.5f,-0.5f,-0.5f,   1.0f, 0.0f,
+         0.5f,-0.5f, 0.5f,   1.0f, 1.0f,
+         0.5f,-0.5f, 0.5f,   1.0f, 1.0f,
+        -0.5f,-0.5f, 0.5f,   0.0f, 1.0f,
+        -0.5f,-0.5f,-0.5f,   0.0f, 0.0f
     };
+
+
 
     unsigned int cubeVAO, cubeVBO;
     glGenVertexArrays(1, &cubeVAO);
@@ -79,11 +116,35 @@ int main() {
     glBindBuffer(GL_ARRAY_BUFFER, cubeVBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(cubeVertices), cubeVertices, GL_STATIC_DRAW);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
+
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
 
     glBindVertexArray(0);
 
+    unsigned int cubeTexture;
+    glGenTextures(1, &cubeTexture);
+    glBindTexture(GL_TEXTURE_2D, cubeTexture);
+
+    // Set parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    // Load image (e.g., using stb_image)
+    int width, height, nrChannels;
+    unsigned char* data = stbi_load("assets/low_res_image.png", &width, &height, &nrChannels, 0);
+    if (data) {
+        GLenum format = (nrChannels == 3) ? GL_RGB : GL_RGBA;
+        glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    } else {
+        std::cout << "Failed to load texture" << std::endl;
+    }
+    stbi_image_free(data);
 
     int mode = 0;
     float fps = 0.0f;
@@ -111,8 +172,11 @@ int main() {
         glViewport(0, 0, SCR_WIDTH, SCR_HEIGHT);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, cubeTexture);
         sceneShader.use();
-        glm::mat4 model = glm::rotate(glm::mat4(1.0f), (float)glfwGetTime(), glm::vec3(0,1,0));
+        sceneShader.setInt("uTexture", 0);
+        glm::mat4 model = glm::rotate(glm::mat4(1.0f), (float)glfwGetTime()/10, glm::vec3(0,1,0));
         glm::mat4 view = glm::lookAt(glm::vec3(2,2,2), glm::vec3(0,0,0), glm::vec3(0,1,0));
         glm::mat4 projection = glm::perspective(glm::radians(45.0f), SCR_WIDTH/(float)SCR_HEIGHT, 0.1f, 100.0f);
         sceneShader.setMat4("model", model);
